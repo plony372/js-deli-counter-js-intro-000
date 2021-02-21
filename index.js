26ff08fab2a7916ae0cc9c80e4440b9af15b9e31
plony benav <menachemfuterfas@gmail.com>
2021-02-21 17:16:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function takeANumber(currntLine, newName){
-  currntLine.push(newName)
-  return `Welcome, ${newName}. You are number ${currntLine.length} in line.`
-} 
+// function takeANumber(currntLine, newName){
+//   currntLine.push(newName)
+//   return `Welcome, ${newName}. You are number ${currntLine.length} in line.`
+// } 
 
-function nowServing(currntLine){
-  if (currntLine[0]) {
-    return `Currently serving ${currntLine.shift()}.`;
-  }else{
-    return "There is nobody waiting to be served!"
-  }
-}
+// function nowServing(currntLine){
+//   if (currntLine[0]) {
+//     return `Currently serving ${currntLine.shift()}.`;
+//   }else{
+//     return "There is nobody waiting to be served!"
+//   }
+// }
 
   function currntLine(katzDeliLine){
     var message = "The line is currently:";
